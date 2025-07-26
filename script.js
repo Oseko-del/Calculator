81862c2dd6e42d7a0ea49846b7d06c3366ba76f3
sydney oseko <osekosydney@gmail.com>
2025-07-26 15:35:05 +0300

    added  DEL button

--- script.js
+++ script.js
@@ -26,3 +26,9 @@ display.value= eval(display.value);
     
     
 }
+
+  function deleteLast() {
+    let display = document.getElementById("display");
+    display.value = display.value.slice(0, -1);
+  }
+
